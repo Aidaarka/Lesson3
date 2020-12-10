5c89f2e5e4988712c9a180e68605d2df6aa8db09
Aidaarka <66637129+Aidaarka@users.noreply.github.com>
2020-12-10 15:38:33 +0300

    Add files via upload

---
+++ Second.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson3.Char
+{
+    class Second
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            string[] words = text.Split(new char[] { ' ' });
+
+            int a = 0;
+            int letter = 0;
+            string result;
+
+            char[] split = text.ToCharArray();
+            int size = split.Length;
+
+            foreach (string s in words)
+            {
+                a = (words.Length - 1);
+            }
+
+            for (int n = 0; n < size; n++)
+            {
+                if (char.IsLetter(split[n])) ++letter;
+            }
+
+            double percent = ((double)a / letter)*100;
+            result = string.Format("{0:0.00}", percent);
+
+            Console.WriteLine("Количество последних букв в тексте составило: {0}%", result);
+        }
+    }
+}
