38e744f0cb56b6785b9806426cfc89e1900d3c60
Aidaarka <66637129+Aidaarka@users.noreply.github.com>
2020-12-10 14:07:32 +0300

    Add files via upload

---
+++ First.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lesson3
+{
+    class First
+    {
+        static void Main(string[] args)
+        {
+            int[] Mas = new int[1000];
+            int CheckNum;
+            int i;
+            bool Check;
+
+            for (CheckNum = 1; CheckNum < 1000; CheckNum++)
+            {
+                Check = true;
+                for (i = 2; i <= CheckNum / 2; i++)
+                {
+                    if (!(Math.Sqrt(CheckNum) < 10) || (CheckNum % i) == 0)
+                    {
+                        Check = false;
+                    }
+                }
+                if (Check)
+                {
+                    Mas[CheckNum] = CheckNum;
+                    Console.Write("{0} ", Mas[CheckNum]);
+                }
+            }
+        }
+    }
+}
