7ac2cd96167701137f0d6316f3a3fdc634d6cfc7
Aidaarka <66637129+Aidaarka@users.noreply.github.com>
2020-12-11 21:03:39 +0300

    Add files via upload

---
+++ Third.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace lesson3.Reged
+{
+    class Third
+    {
+        static void Main()
+        {
+            //Email checking
+            Regex RuleCheckEmail = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Console.WriteLine(RuleCheckEmail.IsMatch("email@mail.ru"));
+
+            //IP checking
+            Regex RuleCheckIP = new Regex(@"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+            Console.WriteLine(RuleCheckIP.IsMatch("0.0.0.0"));
+        }
+    }
+}
